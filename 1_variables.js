cb213db0ff23b6dca31296b36e48555eb95d1871
Dheeraj kumar <58466505+DheerajKumar066@users.noreply.github.com>
2021-10-01 23:18:54 +0530

    Add files via upload

---
+++ 1_variables.js
@@ -0,0 +1,34 @@
+//Variables let,const,var
+/*
+let message;
+message="Hello ,How are you my boy?";
+console.log(message);
+*/
+// let a=10 , b=20;
+// console.log("A= "+a+ "\nB= "+b+"\nA+B= "+(a+b));
+
+/*
+let message="how are you my boy?";
+console.log(message);
+
+
+message="Good to go!";
+const user ="LearnWithDHK";
+console.log(user);
+// user="You are with me!";
+*/
+
+
+/*
+if(4/3)
+{
+    console.log("Inside block of if loop");
+    var address="Baner Pune";
+    console.log(address);
+}
+console.log(address);
+*/
+
+z =56;
+console.log("Value of Z is ", z);
+var z;
