15031c7ad1c11f2ccf4c1a3d1ea6d286e3914a13
Dheeraj kumar <58466505+DheerajKumar066@users.noreply.github.com>
2021-10-06 18:40:48 +0530

    Update Linkedin.js

--- Linkedin.js
+++ Linkedin.js
@@ -1,5 +1,5 @@
 // invitation_action-btn
-document.querySelectorAll(".invitation-card__action-btn");
+let btns = document.querySelectorAll(".invitation-card__action-btn");
 for(let b of btns)
 {
     b.click();
